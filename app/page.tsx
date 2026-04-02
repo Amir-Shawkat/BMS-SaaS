07046a40e4990617a921b5267a3ab8389a7ca41e
amir-shawkat <amirshawkat05@gmail.com>
2026-04-02 14:09:54 +0600

    Implement Homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import BookCard from "@/components/BookCard";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+
 const Page = () => {
   return (
-    <div>
-      {/* <h1 className="text-2xl underline">Bookified</h1> */}
-    </div>
+    <main className="wrapper container">
+      <HeroSection />
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard key={book._id} title={book.title} author={book.author}
+              coverURL={book.coverURL} slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
   );
 };
 
